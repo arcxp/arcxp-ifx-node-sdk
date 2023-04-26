8e16f5407c69229d9e3192d5ab13f0425544e3f4
Infbot IFX <infbot-ifx@washpost.com>
2023-04-26 22:05:07 +0000

    Release v1.2.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# arcxp-ifx-node-sdk
+# Configuration
+## Environment
+The Node SDK allows clients to specify environment specific configuration via 
+[dotenv](https://www.npmjs.com/package/dotenv) files in the root directory of the running 
+application. See expected folder structure below.
+```sh
+.
+├── .env.development
+├── .env.production
+├── .env.sandbox
+├── .gitignore
+├── README.md
+├── package.json
+├── src
+│   ├── eventsHandlers
+│   │   ├── defaultHandler.js
+```
--- lambdaHandler.js
+++ lambdaHandler.js
@@ -1,4 +1,25 @@
-require('dotenv').config();
+const getEnv = () => {
+  if (process.env.ENV === 'sandbox' || process.env.ENV === 'test') {
+    return 'sandbox';
+  } if (process.env.ENV === 'production' || process.env.ENV === 'prod') {
+    return 'production';
+  }
+  return 'development';
+};
+
+const env = getEnv();
+
+// lambdaHandler.js is the entrypoint of all Lambda invocations.
+// Because of this, environment file intiialization should be the first thing that is loaded.
+
+// Load the environment file '.env' containing secrets that the build process generates
+require('dotenv').config({});
+
+// Load client environment files
+console.log('Loading environment configuration for env: ', env);
+require('dotenv').config({
+  path: process.cwd() + '/.env.' + env,
+});
 
 const { sanitizeEnvironment } = require('./src/preHandle');
 
